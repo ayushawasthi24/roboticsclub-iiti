a84dee8c3a20c8eab9c5c1db2cfc42e2639a3837
Ayush Kumar Awasthi <116417909+ayushawasthi24@users.noreply.github.com>
2024-06-29 01:16:39 +0530

    changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-gray-900 text-white text-center py-4">
       <div className="container mx-auto">
         <p>&copy; 2024 Robotics Club, IIT Indore. All rights reserved.</p>
-        <p>Designed by Your Name</p>
+        <p>Designed by Ayush Awasthi</p>
       </div>
     </footer>
   );
